3c143aed022335ed8a241ecc4b739dafa44a7e3a
Developer <developer@192.168.1.35>
2023-11-28 15:49:23 +0500

    change googleservices.info

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,15 @@
 import UIKit
 import Flutter
-import GooglePlaces
+// import GooglePlaces
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
+//     AppEvents.activateApp()
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCtPxGy4YRi5KNcJYpgLHT0AgQurY5fZ9g")
+//     GMSServices.provideAPIKey("AIzaSyCtPxGy4YRi5KNcJYpgLHT0AgQurY5fZ9g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
